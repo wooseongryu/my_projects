2e193799ebf1a72b487ab9a5fb338401f94c6278
wryu <useong3754@gmail.com>
2022-10-19 20:16:46 +0900

    Post모델 추가 및 url작성

---
+++ board/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path(
+    #     'list/',
+    #     views.post_list.as_view(),
+    #     name='post-list'
+    # ),
+    # path(
+    #     'write/',
+    #     views.post_write.as_view(),
+    #     name='post-write'
+    # ),
+    # path(
+    #     'post/<int:page_id>/',
+    #     views.post_detail.as_view(),
+    #     name='post-detail'
+    # ),
+    # path(
+    #     'post/<int:page_id>/edit/',
+    #     views.post_update.as_view(),
+    #     name='post-update'
+    # ),
+    # path(
+    #     'post/<int:page_id>/delete/',
+    #     views.post_delete.as_view(),
+    #     name='post-delete'
+    # ),
+]

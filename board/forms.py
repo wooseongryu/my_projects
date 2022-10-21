5ca1cc376f46929bc08d151aa7dd96063104bc26
wryu <useong3754@gmail.com>
2022-10-21 20:57:05 +0900

    회원가입시 추가 필드 나타내기

--- board/forms.py
+++ board/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Post
+from .models import Post, User
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["nickname"]
+
+    def signup(self, request, user):
+        user.nickname = self.cleaned_data["nickname"]
+        user.save()
+    

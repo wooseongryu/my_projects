e13aaab0659fb2b3e7577f13beed84dff1a5300e
wryu <useong3754@gmail.com>
2022-10-28 20:31:36 +0900

    Like 인라인 사용

--- board/admin.py
+++ board/admin.py
@@ -3,10 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.contenttypes.admin import GenericStackedInline  
 from .models import Post, User, Comment, Like
 
+
 class CommentInline(admin.StackedInline):
     model = Comment
 
-    
+
 """
 Like모델은 GenericForeignKey를 사용했으니
 GenericStackedInline를 사용해야 에러가 나지 않는다
@@ -24,8 +25,14 @@ class PostAdmin(admin.ModelAdmin):
         LikeInline,
     )
 
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
+
 # 기본으로 제공하는 UserAdmin
 admin.site.register(User, UserAdmin)
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
+# 따로 작성하지 않으면 기본으로 ModelAdmin이 사용된다
 admin.site.register(Like)
